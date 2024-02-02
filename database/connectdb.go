58c29a5955391d607797811b62fe39a1d76df505
Sangeeth <sangeethsajeev518@gmail.com>
2024-02-02 16:09:50 +0530

    added databse

---
+++ database/connectdb.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/sangeeth518/learning-routes/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectTODb() {
+	var err error
+
+	dsn := os.Getenv("dsn")
+
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to DB")
+	}
+	log.Println("Database connected succesfully")
+
+	DB.AutoMigrate(&models.User{})
+}
